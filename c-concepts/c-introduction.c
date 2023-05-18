6a401b60480ac1891d0836453d72d34e22b6a6ed
FranklinMiranda <fdm61344@gmail.com>
2023-05-17 21:26:51 -0400

    Commit

---
+++ c-concepts/c-introduction.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+// Comment in c
+/* Multiline Comment in 
+c
+*/
+int main() { 
+	
+	int x = 102;
+	float y = 10.1;
+	int z = 3;
+	double xz = .123456789;
+ 
+	x = y = z = 5;
+
+	int a = x;
+	float b;
+	char c = 'c';
+
+	b = 120.01233123;
+
+	float sum = x + b;
+
+	printf("\nThis letter is %c and this number is %f \n\n", c, sum);
+
+	printf("%.20lf", xz);
+	
+	printf("\n\n%d", x);
+	printf("\n%f", y);
+	printf("\n%d", z);
+
+	printf("\nHello World!");
+			
+printf("\n\n1,2,3,4,5,6,7,8,9")
+;return 0; }
+
+
+
